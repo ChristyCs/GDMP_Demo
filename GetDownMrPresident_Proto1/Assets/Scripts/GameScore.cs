cd966cc66c427319fe4156a8211a56dd8ec79407
Michael <michaelcpgordon@gmail.com>
2016-05-24 12:03:30 +1200

    Added player score variables/functions

--- GetDownMrPresident_Proto1/Assets/Scripts/GameScore.cs
+++ GetDownMrPresident_Proto1/Assets/Scripts/GameScore.cs
@@ -9,10 +9,15 @@ public class GameScore : MonoBehaviour {
     int mins;
     string durationString;
 
+    int bodyGuardScore;
+    int assassinScore;
+
 
     // Use this for initialization
     void Start () {
         updateDuration();
+        bodyGuardScore = 0;
+        assassinScore = 0;
     }
 	
 	// Update is called once per frame
@@ -22,11 +27,18 @@ public class GameScore : MonoBehaviour {
         print(durationString);
     }
 
-    void updateDuration()
-    {
+    void updateDuration(){
         duration = (int)Time.time;
         seconds = duration % 60;
         mins = (duration - mins) / 60;
         durationString = mins.ToString("00") + ":" + seconds.ToString("00");
+    }
+
+    public void addTobodyGuardScore(int points){
+        bodyGuardScore += points;
+    }
+
+    public void addToAssassinScore(int points){
+        assassinScore += points;
     }
 }
